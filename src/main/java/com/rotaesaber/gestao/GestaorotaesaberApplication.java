e8a3b729ad7b568122a81348ef553a58486166ff
othiagotenorio2 <thiago.tenorio@cotefacil.com>
2025-08-20 14:02:58 -0300

    Projeto: Rota e Saber (GESTÃO)

---
+++ src/main/java/com/rotaesaber/gestao/GestaorotaesaberApplication.java
@@ -0,0 +1,13 @@
+package com.rotaesaber.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaorotaesaberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaorotaesaberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rotaesaber/gestao/GestaorotaesaberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rotaesaber.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaorotaesaberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
